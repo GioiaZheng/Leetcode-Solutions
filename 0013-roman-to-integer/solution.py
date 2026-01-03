95e263a474534b1754192d449a610900095e6be2
Gioia Zheng <161201041+GioiaZheng@users.noreply.github.com>
2026-01-03 10:50:41 +0100

    Create solution.py

---
+++ 0013-roman-to-integer/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        value = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        n = len(s)
+
+        for i in range(n):
+            # If the next symbol has a larger value, subtract current
+            if i + 1 < n and value[s[i]] < value[s[i + 1]]:
+                total -= value[s[i]]
+            else:
+                total += value[s[i]]
+
+        return total
