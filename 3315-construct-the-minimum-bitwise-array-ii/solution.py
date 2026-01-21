a005f46a98e6d594a77d4305d7118e33dab9b389
Gioia Zheng <161201041+GioiaZheng@users.noreply.github.com>
2026-01-21 23:11:33 +0100

    Add minBitwiseArray method to Solution class
    Implement minBitwiseArray method to compute a minimum bitwise array based on input conditions.

---
+++ 3315-construct-the-minimum-bitwise-array-ii/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for n in nums:
+            # Even prime (only possible is 2)
+            if n % 2 == 0:
+                res.append(-1)
+                continue
+
+            pos = 0
+            # Find the lowest zero bit
+            while (n >> pos) & 1:
+                pos += 1
+
+            res.append(n - (1 << (pos - 1)))
+
+        return res
